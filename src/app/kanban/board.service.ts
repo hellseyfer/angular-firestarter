32b3f20a969f06c9fa0bce91c7236efd949cb1bd
Juan Montilla <juan.montilla@southworks.com>
2024-12-06 09:46:54 -0300

    fix build issue

--- src/app/kanban/board.service.ts
+++ src/app/kanban/board.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { AngularFireAuth } from "@angular/fire/compat/auth";
 import { AngularFirestore } from "@angular/fire/compat/firestore";
-import firebase from "firebase/app";
+import firebase from "firebase/compat/app";
 import { switchMap } from "rxjs/operators";
 import { Board, Task } from "./board.model";
 
