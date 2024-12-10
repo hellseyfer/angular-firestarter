454080b4d4525bdcafd8e8efc0ab974d44c81870
Juan Montilla <juan.montilla@southworks.com>
2024-12-10 11:09:01 -0300

    envs and favicon

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,11 @@
 export const environment = {
   production: true,
-  firebase:  {
-    apiKey: 'AIzaSyAKlEX5a8PQaadYNSrohqfl33O37Kjkv1Y',
-    authDomain: 'angular-voxer.firebaseapp.com',
-    databaseURL: 'https://angular-voxer.firebaseio.com',
-    projectId: 'angular-voxer',
-    storageBucket: 'angular-voxer.appspot.com',
-    messagingSenderId: '122050526979',
-    appId: '1:122050526979:web:529304dce5ffca7a'
-  }
+  firebase: {
+    apiKey: "AIzaSyCUxatwZD7xKvDohCEK25raP9K45zTiAVo",
+    authDomain: "angular-kanban-9875c.firebaseapp.com",
+    projectId: "angular-kanban-9875c",
+    storageBucket: "angular-kanban-9875c.firebasestorage.app",
+    messagingSenderId: "651342070437",
+    appId: "1:651342070437:web:490cfb190d817b8ac1034c",
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,14 @@
 
 export const environment = {
   production: false,
-  firebase:  {
-    apiKey: 'AIzaSyAKlEX5a8PQaadYNSrohqfl33O37Kjkv1Y',
-    authDomain: 'angular-voxer.firebaseapp.com',
-    databaseURL: 'https://angular-voxer.firebaseio.com',
-    projectId: 'angular-voxer',
-    storageBucket: 'angular-voxer.appspot.com',
-    messagingSenderId: '122050526979',
-    appId: '1:122050526979:web:529304dce5ffca7a'
-  }
+  firebase: {
+    apiKey: "AIzaSyCUxatwZD7xKvDohCEK25raP9K45zTiAVo",
+    authDomain: "angular-kanban-9875c.firebaseapp.com",
+    projectId: "angular-kanban-9875c",
+    storageBucket: "angular-kanban-9875c.firebasestorage.app",
+    messagingSenderId: "651342070437",
+    appId: "1:651342070437:web:490cfb190d817b8ac1034c",
+  },
 };
 
 /*
